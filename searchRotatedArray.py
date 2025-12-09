bb3f178dd424d5deb12464e5915bd77cc8d05e96
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-12-09 09:30:35 -0600

    Update searchRotatedArray.py

--- searchRotatedArray.py
+++ searchRotatedArray.py
@@ -16,3 +16,9 @@ class Solution:
                     l = mid + 1
                 else:
                     r = mid - 1
+            else:
+                if target < nums[mid] or target > nums[r]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+        return -1
